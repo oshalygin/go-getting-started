ffe0b07fda10c3a53af60f862b69cc49b2ad62b7
oshalygin <oshalygin@gmail.com>
2017-05-12 17:59:07 -0700

    Spiking on types

--- src/main.go
+++ src/main.go
@@ -1,10 +1,23 @@
 package main
 
-var (
-	message = "foobar"
-)
-
 func main() {
-	name := "Master of all things fun"
-	println(name)
+	var myInt int
+	myInt = 42
+
+	println(myInt)
+
+	var myFloat32 float32
+	myFloat32 = 33.22
+
+	println(myFloat32)
+
+	myString := "Hello Go!"
+
+	println(myString)
+
+	myComplex := complex(2, 3)
+
+	println(myComplex)
+	println(real(myComplex), imag(myComplex))
+
 }
