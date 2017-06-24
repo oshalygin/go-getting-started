35033de0961451f0eaf1861bfce991ef54aa9b37
oshalygin <oshalygin@gmail.com>
2017-06-24 00:42:48 -0700

    Add tasks

--- src/main.go
+++ src/main.go
@@ -2,22 +2,44 @@ package main
 
 func main() {
 	var myInt int
-	myInt = 42
-
+	myInt = 1337
 	println(myInt)
-
-	var myFloat32 float32
-	myFloat32 = 33.22
-
-	println(myFloat32)
-
-	myString := "Hello Go!"
-
-	println(myString)
-
-	myComplex := complex(2, 3)
-
-	println(myComplex)
-	println(real(myComplex), imag(myComplex))
-
 }
+
+// package main
+
+// const (
+// 	first = iota
+// 	second
+// )
+
+// const (
+// 	third = iota
+// )
+
+// func main() {
+
+// 	println(first)
+// 	println(second)
+// 	println(third)
+
+// 	var myInt int
+// 	myInt = 42
+
+// 	println(myInt)
+
+// 	var myFloat32 float32
+// 	myFloat32 = 33.22
+
+// 	println(myFloat32)
+
+// 	myString := "Hello Go!"
+
+// 	println(myString)
+
+// 	myComplex := complex(2, 3)
+
+// 	println(myComplex)
+// 	println(real(myComplex), imag(myComplex))
+
+// }
