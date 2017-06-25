cc1e2c92f1ee3fca1fbe1be38fa0f8654bd36207
oshalygin <oshalygin@gmail.com>
2017-06-25 16:30:36 -0700

    Refactor into functions

--- src/main.go
+++ src/main.go
@@ -4,6 +4,39 @@ import (
 	"fmt"
 )
 
+// func calculateCapacity(activePlants []int, plantCapacities []float64) float64 {
+// 	capacity := 0.
+// 	for _, plantID := range activePlants {
+// 		capacity += plantCapacities[plantID]
+// 	}
+// 	return capacity
+// }
+
+// func getUtilization(capacity float64, load float64) float64 {
+// 	return capacity / load * 100
+// }
+
+func getCapacity(activePlants []int, plantCapacities []float64) func(float64) float64 {
+	capacity := 0.
+	for _, plantID := range activePlants {
+		capacity = capacity + plantCapacities[plantID]
+	}
+
+	return func(gridLoad float64) float64 {
+		return capacity / gridLoad * 100
+	}
+}
+
+func getUtilization(capacity float64, gridLoad float64) float64 {
+	return capacity / gridLoad * 100
+}
+
+func printOutput(capacity float64, load float64, utilization float64) {
+	fmt.Printf("%-20s%v\n", "Capacity: ", capacity)
+	fmt.Printf("%-20s%v\n", "Load: ", load)
+	fmt.Printf("%-20s%.1f %%\n", "Utilization: ", utilization)
+}
+
 func main() {
 
 	plantCapacities := []float64{30, 30, 30, 60, 60, 100}
@@ -24,18 +57,18 @@ func main() {
 			fmt.Printf("Plant %d capacity: %.0f\n", index, capacity)
 		}
 	case "2":
-		capacity := 0.
-		for _, plantID := range activePlants {
-			capacity += plantCapacities[plantID]
-		}
+		// capacity := calculateCapacity(activePlants, plantCapacities)
+		// utilization := getUtilization(capacity, gridLoad)
+		capacity := getCapacity(activePlants, plantCapacities)
+		utilizationOne := capacity(gridLoad)
+		utilizationTwo := capacity(100.0)
+		fmt.Printf("%v\n", utilizationOne)
+		fmt.Printf("%v\n", utilizationTwo)
 
-		utilization := capacity / gridLoad * 100
+		// printOutput(capacity, gridLoad, utilization)
 
-		// Output
-		fmt.Printf("%-20s%v\n", "Capacity: ", capacity)
-		fmt.Printf("%-20s%v\n", "Load: ", gridLoad)
-		fmt.Printf("%-20s%.1f %%\n", "Utilization: ", utilization)
 	default:
 		fmt.Println("I have no idea what you entered")
 	}
+
 }
