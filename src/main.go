291d27cef99f481490213f51dba835129a95ebdf
oshalygin <oshalygin@gmail.com>
2017-06-25 15:20:53 -0700

    WIP PowerPlant loops

--- src/main.go
+++ src/main.go
@@ -1,51 +1,38 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
 
-	if foo := 2; foo == 4 {
-		println("foobar")
-	} else {
-		println("more foobars")
-	}
+	plantCapacities := []float64{30, 30, 30, 60, 60, 100}
+	activePlants := []int{0, 1}
+	gridLoad := 75.
 
-	baz := 2
-	switch baz {
-	case 1:
-		{
-			println("bazzing")
+	fmt.Println("1) Generate Power Plant Report")
+	fmt.Println("2) Generate power Grid Report")
+	fmt.Print("Please choose an option:")
+
+	var option string
+
+	fmt.Scanln(&option)
+
+	switch option {
+	case "1":
+		for index, capacity := range plantCapacities {
+			fmt.Printf("Plant %d capacity: %.0f\n", index, capacity)
 		}
-	case 2:
-		{
-			println("lol baz")
+	case "2":
+		capacity := 0.
+		for _, plantID := range activePlants {
+			capacity += plantCapacities[plantID]
 		}
+
+		utilization := capacity / gridLoad * 100
+		fmt.Printf("%-20s%.0f\n", "Capacity: ", capacity)
+		fmt.Printf("%-20s%.0f\n", "Load: ", gridLoad)
+		fmt.Printf("%-20s%.1f\n", "Utilization: ", utilization)
 	}
 
-	for iterator := 0; iterator < 5; iterator++ {
-		println(iterator)
-	}
-
-	j := 0
-	for {
-		j++
-		println(j)
-		if j == 5 {
-			break
-		}
-	}
-
-	slice := []string{"foo", "bar", "baz"}
-
-	for index, value := range slice {
-		println(index, value)
-	}
-
-	dictionary := make(map[string]string)
-
-	dictionary["first"] = "foo"
-	dictionary["second"] = "bar"
-	dictionary["third"] = "baz"
-
-	for key, value := range dictionary {
-		println(key, value)
-	}
 }
