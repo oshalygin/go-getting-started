f02c49afa0852ede4f8a56d566ab3d6a44c9f1b6
oshalygin <oshalygin@gmail.com>
2017-06-25 21:02:38 -0700

    Concurrency

--- src/main.go
+++ src/main.go
@@ -1,78 +1,34 @@
 package main
 
 import (
-	"fmt"
-	"strings"
+	"runtime"
 )
 
 func main() {
+	runtime.GOMAXPROCS(8)
 
-	plants := []PowerPlant{
-		PowerPlant{hydro, 300, active},
-		PowerPlant{wind, 30, active},
-		PowerPlant{wind, 25, inactive},
-		PowerPlant{hydro, 35, active},
-		PowerPlant{hydro, 40, active},
+	channel := make(chan string)
+	doneChannel := make(chan bool)
+
+	go letterGenerator(channel)
+	go printer(channel, doneChannel)
+
+	println("Stuff is happening")
+	<-doneChannel
+}
+
+func letterGenerator(ch chan string) {
+	for letter := byte('a'); letter <= byte('z'); letter++ {
+		ch <- string(letter)
 	}
 
-	grid := PowerGrid{1000, plants}
-
-	grid.generatePowerPlantReport()
-	grid.generateGridReport()
+	close(ch)
 }
 
-type PlantType string
-type PlantStatus string
-
-const (
-	hydro PlantType = "Hydro"
-	wind  PlantType = "Wind"
-	solar PlantType = "Solar"
-)
-
-const (
-	active      PlantStatus = "Active"
-	inactive    PlantStatus = "Inactive"
-	unavailable PlantStatus = "Unavailable"
-)
-
-type PowerPlant struct {
-	plantType PlantType
-	capacity  float64
-	status    PlantStatus
-}
-
-type PowerGrid struct {
-	load   float64
-	plants []PowerPlant
-}
-
-func (pg *PowerGrid) generatePowerPlantReport() {
-	for index, plant := range pg.plants {
-		label := fmt.Sprintf("%s%d", "Plant #", index)
-		fmt.Println(label)
-		fmt.Println(strings.Repeat("-", len(label)))
-		fmt.Printf("%-20s%s\n", "Type: ", plant.plantType)
-		fmt.Printf("%-20s%.0f\n", "Capacity: ", plant.capacity)
-		fmt.Printf("%-20s%s\n", "Status: ", plant.status)
-		fmt.Printf("\n")
-	}
-}
-
-func (pg *PowerGrid) generateGridReport() {
-	capacity := 0.00
-	for _, plant := range pg.plants {
-		if plant.status == active {
-			capacity = capacity + plant.capacity
-		}
+func printer(ch chan string, done chan bool) {
+	for letter := range ch {
+		println(letter)
 	}
 
-	utilization := capacity / pg.load * 100
-
-	label := "Power Grid Report"
-	fmt.Println(label)
-	fmt.Println(strings.Repeat("-", len(label)))
-	fmt.Printf("%-20s%.0f\n", "Total Capacity: ", capacity)
-	fmt.Printf("%-20s%.0f\n", "Grid Load: ", pg.load)
-	fmt.Printf("%-20s%.0f %% \n", "Utilization: ", utilization)
+	done <- true
 }
